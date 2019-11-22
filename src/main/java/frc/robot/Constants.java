cf5076be6ef77ef28da55eb689ea5d5bfd651e3d
Programming Student <prog@chaos.local>
2019-11-21 20:16:33 -0500

    First Commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,35 @@
+package frc.robot;
+
+public class Constants {
+	public static final int
+
+	REFRESH_RATE = 5,
+
+			// PWM
+			ARM_M = 6 , 
+			CAM_SERV = 9,
+			INTAKE = 5,
+			CLIMBER = 1,
+			CLIMBER2 = 0,
+
+			//CAN 
+			DRIVE_FL = 10,
+			DRIVE_FR = 12,
+			DRIVE_BL = 11,
+			DRIVE_BR = 13,
+			// DIO
+			//DRIVE_ENCODER_LA = , DRIVE_ENCODER_RA = , DRIVE_ENCODER_LB = , DRIVE_ENCODER_RB = ,
+			INTAKE_LIMIT = 0, // NOT FINAL
+
+			// Analog Input
+			IR_PORT = 0,
+			ARM_POT = 1,
+
+			//ARM_MIN_DEG = , ARM_MAX_DEG = ,
+
+			// Solenoid
+			GEARBOX_PNU = 2, ARM_PNU_CLOSE = 0, ARM_PNU_OPEN = 1; // open / close may not be in the correct order
+
+	
+
+}
